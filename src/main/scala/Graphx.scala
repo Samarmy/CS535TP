23cf02be54759614f81789853b59285ec4be1048
Samarmy <1samarmstrong@gmail.com>
2019-04-29 19:54:35 -0600

    Update Graphx.scala

--- src/main/scala/Graphx.scala
+++ src/main/scala/Graphx.scala
@@ -4,6 +4,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.graphx._
 import org.apache.spark.rdd.RDD
 import scala.util.Try
+import java.util.Date
 import org.apache.commons.io.FileUtils
 import com.google.gson.Gson
 import java.io.{InputStream, OutputStream, File}
@@ -16,8 +17,7 @@ import java.net.InetSocketAddress
 
 class VertexProperty()
 case class tweet(hashtag:String,screen_name:String,time:Long,quote_count:Int,reply_count:Int,retweet_count:Int,favorite_count:Int,favorited:Boolean,retweeted:Boolean,filter_level:String) extends VertexProperty
-case class user(screen_name:String,followers_count:Int,friends_count:Int,listed_count:Int,time:Long,favourites_count:Int,time_zone:String,geo_enabled:Boolean,verified:Boolean,statuses_count:Int,contributors_enabled:Boolean,is_translator:Boolean,profile_use_background_image:Boolean,default_profile:Boolean,default_profile_image:Boolean,following:Boolean,follow_request_sent:Boolean,notifications:Boolean,translator_type:String) extends VertexProperty
-
+case class user(screen_name:String,followers_count:Int,friends_count:Int,listed_count:Int,time:Long,favourites_count:Int,time_zone:String,geo_enabled:Boolean,verified:Boolean,statuses_count:Int,contributors_enabled:Boolean,is_translator:Boolean,profile_use_background_image:Boolean,default_profile:Boolean,default_profile_image:Boolean,following:Boolean,follow_request_sent:Boolean,notifications:Boolean,translator_type:String,labels:Map[String, Array[Long]],hashtags:Array[tweet],prediction:Double) extends VertexProperty
 case class JsonHolder(j: String){
     var json = j
 }
@@ -33,7 +33,7 @@ object Graphx {
         import spark.implicits._
         val sc = spark.sparkContext
 
-        val userHashtags = spark.read.textFile("hdfs://austin:30121/test/userHashtags/*").rdd.map(x => {
+        var userHashtags = spark.read.textFile("hdfs://austin:30121/test/userHashtags/*").rdd.map(x => {
           var strAry = x.split(",")
           var props = strAry(1).substring(0, strAry(1).length - 1).split(" ")
           (strAry(0).substring(1) + " t", tweet(props(0), strAry(0).substring(1), props(1).toLong,  props(2).toInt, props(3).toInt, props(4).toInt, props(5).toInt, Try(props(6).toBoolean).getOrElse(false), Try(props(7).toBoolean).getOrElse(false), props(8)).asInstanceOf[Any])
@@ -42,14 +42,22 @@ object Graphx {
         val userData = spark.read.textFile("hdfs://austin:30121/test/userData/*").rdd.map(x => {
           var strAry = x.split(",")
           var props = strAry(1).substring(0, strAry(1).length - 1).split(" ")
-          (strAry(0).substring(1) + " u", user(strAry(0).substring(1), props(0).toInt, props(1).toInt, props(2).toInt, props(3).split("\\.")(0).toLong, props(4).toInt, props(5), Try(props(6).toBoolean).getOrElse(false), Try(props(7).toBoolean).getOrElse(false), props(8).toInt, Try(props(9).toBoolean).getOrElse(false), Try(props(10).toBoolean).getOrElse(false), Try(props(11).toBoolean).getOrElse(false), Try(props(12).toBoolean).getOrElse(false), Try(props(13).toBoolean).getOrElse(false), Try(props(14).toBoolean).getOrElse(false), Try(props(15).toBoolean).getOrElse(false), Try(props(16).toBoolean).getOrElse(false), props(17)).asInstanceOf[Any])
+          (strAry(0).substring(1) + " u", user(strAry(0).substring(1), props(0).toInt, props(1).toInt, props(2).toInt, props(3).split("\\.")(0).toLong, props(4).toInt, props(5), Try(props(6).toBoolean).getOrElse(false), Try(props(7).toBoolean).getOrElse(false), props(8).toInt, Try(props(9).toBoolean).getOrElse(false), Try(props(10).toBoolean).getOrElse(false), Try(props(11).toBoolean).getOrElse(false), Try(props(12).toBoolean).getOrElse(false), Try(props(13).toBoolean).getOrElse(false), Try(props(14).toBoolean).getOrElse(false), Try(props(15).toBoolean).getOrElse(false), Try(props(16).toBoolean).getOrElse(false), props(17), Map.empty[String, Array[Long]], Array[tweet](), 0.0))
         }).reduceByKey((v1, v2) => {
           if(v1.asInstanceOf[user].time > v2.asInstanceOf[user].time){
             v1
           }else{
             v2
           }
-        }).cache()
+        }).join(userHashtags).map(x => {
+          var tweet_data = x._2._2.asInstanceOf[tweet]
+          var str = tweet_data.hashtag
+          var lng = tweet_data.time
+          // user_data.hashtags :+ tweet_data
+          (x._1, x._2._1.asInstanceOf[user].copy(labels=Map(str -> Array(lng)), hashtags=Array(tweet_data)).asInstanceOf[Any])
+        }).reduceByKey((v1, v2) => v1.asInstanceOf[user].copy(labels=(v1.asInstanceOf[user].labels ++ v2.asInstanceOf[user].labels), hashtags = (v1.asInstanceOf[user].hashtags ++ v2.asInstanceOf[user].hashtags))).map(x => (x._1 + " u", x._2)).cache()
+        
+        userHashtags = userHashtags.map(x => (x._1 + " t", x._2))
         //(LeadingWPassion,50495 49713 282 1445416821000.0 15299 None False False 434339 False False True False False None None None none)
         val userRelations = spark.read.textFile("hdfs://austin:30121/test/userRelations/*").rdd.map(x => {
           var strAry = x.split(",")
