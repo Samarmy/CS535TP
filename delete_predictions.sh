74cb4a69b933c6b081759d5761b618ef7fb9f7f1
Samarmy <1samarmstrong@gmail.com>
2019-05-07 22:53:49 -0600

    Add files via upload

---
+++ delete_predictions.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#https://spark.apache.org/docs/latest/submitting-applications.html
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsPolitics
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsFunny
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsWin
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsHappybirthday
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsMetoo
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsPhotography
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsMarvel
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsPets
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsFriends
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsScience
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsBirthday
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsTech
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsTechnology
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsFashion
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsTrump
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsImpeachdonaldtrump
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsNews
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsFakenews
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsFamily
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsFood
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsSummer
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsUsa
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsLove
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsMen
+
+$HADOOP_HOME/bin/hadoop fs -rm -r /tp/predictionsWomen
